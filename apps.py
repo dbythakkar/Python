c70df6e721b76ec95158b069ab125ebd89efdd61
Yash Thakkar <ythakkar@directbuy.com>
2017-03-13 01:02:10 -0500

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class DbwebappConfig(AppConfig):
-    name = 'DBWebApp'
+class PersonalConfig(AppConfig):
+    name = 'personal'
--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
 from . import views
-
 urlpatterns = [
-    url(r'^$', views.index, name= 'index')]
+    url(r'^admin/', admin.site.urls),
+    url(r'^DBWebApp/', include('DBWebApp.urls')),
+    url(r'^$', views.index, name='index'),
+]
